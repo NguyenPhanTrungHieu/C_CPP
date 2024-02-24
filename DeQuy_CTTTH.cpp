894a59a84ad454ad2e821e38977a13740e637089
NguyenPhanTrungHieu <nptrunghieu1809gm@gmail.com>
2024-02-24 10:11:59 +0700

    24/02/24

---
+++ DeQuy_CTTTH.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+float A1(int n);
+float A2(int n);
+float A4(int n);
+
+int main()
+{
+	int n = 4;
+	printf("\nBai 1:");
+	printf("\nDe Quy: %.3f",A1(n));
+	printf("\nCTTQ : %.3f ", pow(1.5,n) ); //CTTQ = (3/2)^n
+
+	printf("\nBai 2:");
+	printf("\nDe Quy: %.3f",A2(n));
+	printf("\nCTTQ : %.3f ", (5.0 - 4.0*pow(0.5,n)) ); //CTTQ = 5 - 4*(1/2)^n
+
+	printf("\nBai 3:");
+	printf("\nDe Quy: Khong code duoc");
+	printf("\nCTTQ : %.3f ", (2.0 + (0.666667)*n)*pow(1.5,n) ); //CTTQ = (2+(2/3)*n)*(3/2)^n
+
+	printf("\nBai 4:");
+	printf("\nDe Quy: %.3f",A4(n));
+	printf("\nCTTQ : %.3f ", (-0.083334)*pow(-2,n) + (0.75)*pow(2,n) - (1.666667)) ; //CTTQ = (-1/12)*(-2)^n + (3/4)*2^n - 5/3
+}
+
+float A1(int n)
+{
+	if(n==0) return 1.0;
+	else return A1(n-1)*1.5;
+}
+
+float A2(int n)
+{
+	if(n==0) return 1;
+	else if(n==1) return 3;
+	else return (3.0*A2(n-1)-A2(n-2))/2.0;
+}
+
+float A4(int n)
+{
+	if(n==0) return -1.0;
+	else if(n==1) return 0;
+	else if(n==2) return 1;
+	else return A4(n-1)+4.0*A4(n-2)-4.0*A4(n-3);
+}
--- Testtt.cpp
+++ Testtt.cpp
@@ -5,7 +5,7 @@ void DeQuy(int n, int array[][n], int temp) {
         if (temp == 0) {
             DeQuy(n - 1, array, n); // Recur with the previous row
         } else {
-            printf("%d ", array[n][temp - 1]); // Print the element
+            printf("%d ", array[n - 1][temp - 1]); // Print the element
             DeQuy(n, array, temp - 1); // Recur with the previous column
         }
     }
@@ -13,9 +13,15 @@ void DeQuy(int n, int array[][n], int temp) {
 
 int main() {
     int n = 2;
-    int Main[n][n] = {{1, 2}, {3, 4}};
+    int Main[n][n]; 
 
-    DeQuy(n, Main, n); // Start recursion
+
+    Main[0][0] = 1;
+    Main[0][1] = 2;
+    Main[1][0] = 3;
+    Main[1][1] = 4;
+
+    DeQuy(n, Main, n); 
 
     return 0;
 }
