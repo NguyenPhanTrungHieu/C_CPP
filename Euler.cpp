9bc33860fc14b56be5830ace2b52c695dd12a9a8
NguyenPhanTrungHieu <nptrunghieu1809gm@gmail.com>
2024-03-06 10:43:13 +0700

    6/3/24

--- Euler.cpp
+++ Euler.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
-#include <fstream>
 using namespace std;
 
 void taoMaTran(int array[][100], int &n);
 void inMaTran(int array[][100], int n, int m);
-// void DFS(int array[][100], int n, int u);
-// int chuaXet[13] = {1,1,1,1,1,1,1,1,1,1,1,1,1};
 int demBacLe(int array[][100], int n);
 int loaiDoThi(int array[][100], int n);
+// void eulerCircle(int array[][100], int n )
 int main()
 {
     int array[100][100], n;
     taoMaTran(array, n);
     inMaTran(array, n, n);
-    cout<<"So bac le cua ma tran la: "<<demBacLe(array,n);
-    // DFS(array, n, 0);
+    cout << "So bac le cua ma tran la: " << demBacLe(array, n);
+    if (loaiDoThi(array, n) == 1)
+        cout << "\nLa Euler";
+    else if (loaiDoThi(array, n) == 0.5)
+        cout << "\nLa 1/2 Euler";
+    else if (loaiDoThi(array, n) == 0)
+        cout << "\nKhong phai Euler";
 }
 
 void taoMaTran(int array[][100], int &n)
@@ -26,7 +29,7 @@ void taoMaTran(int array[][100], int &n)
         fi >> n;
         for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < n; j++) 
+            for (int j = 0; j < n; j++)
             {
                 fi >> array[i][j];
             }
@@ -45,7 +48,7 @@ void inMaTran(int array[][100], int n, int m)
     {
         for (int j = 0; j < m; j++)
         {
-        	cout << array[i][j] << " ";
+            cout << array[i][j] << " ";
         }
         cout << endl;
     }
@@ -54,19 +57,26 @@ void inMaTran(int array[][100], int n, int m)
 int demBacLe(int array[][100], int n)
 {
     int count = 0;
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
         int tempCount = 0;
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            if(array[i][j] == 1) tempCount++;
+            if (array[i][j] == 1)
+                tempCount++;
         }
-        if(tempCount%2==1) count++;
+        if (tempCount % 2 == 1)
+            count++;
     }
     return count;
 }
 
 int loaiDoThi(int array[][100], int n)
 {
-    int Stack[100], CE[100];
+    if (demBacLe(array, n) % 2 == 0)
+        return 1;
+    if (demBacLe(array, n) > 2)
+        return 0;
+    else
+        return 0.5;
 }
